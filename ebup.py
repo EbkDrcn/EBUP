a5288311acbe66c2bccf4e60ae28bb6642ac705d
Ebubekir Dirican <67467774+EbkDrcn@users.noreply.github.com>
2026-04-24 18:37:58 +0300

    constants are moved to a different file to improve main file

--- ebup.py
+++ ebup.py
@@ -2,24 +2,12 @@ import socket
 import json
 import threading
 import time
+
 import handlers
+from constants import EBUPConstants
 
-class EBUProtocol :
-    defaultPort = 1302
-    starterBit = "EBUP-S-v1"
-    enderBit = "EBUP-E-v1"
-    version = 1
+class EBUProtocol(EBUPConstants) :
     doNotDisturb = False
-
-    ackQuery = {"type":"ack_query", "data":"ack_check"}
-    ackAffirmative = {"type":"ack_query", "data":"ack_affirmative"}
-    ackNegative = {"type":"ack_query", "data":"ack_negative"}
-    msgRecieved = {"type":"msgInfo", "data":"msg_recieved"}
-    msgTypeError = {"type":"msgInfo", "data":"msg_returned_type_error"}
-    discoverySearch = {"type":"discovery", "data":"discovery"}
-    discoveryAns = {"type":"discovery", "data":"discovery_here"}
-    versionCheck = {"type":"discovery", "data":"versionCheck"}
-    message = {"type":"msg", "data":"", "priority":False}
 
     addressBook = []
 
